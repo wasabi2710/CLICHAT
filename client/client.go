874094a94c361a881ee27efc28aefb9b8bdc16fa
wasabi2710 <nyle.hawking@gmail.com>
2024-10-26 20:51:04 +0700

    new mod

--- client/client.go
+++ client/client.go
@@ -29,10 +29,17 @@ type Message struct {
 // connection to clichat
 var conn net.Conn
 
-// handler: server incoming connection
-func handleIncomingMessage(conn net.Conn, message *tview.TextView, clientList *tview.Form) {
-	defer conn.Close()
+// available clients list
+var _clients []string
 
+// handler: clients selection
+func clientSelect(index int, mainText string, secondaryText string, shortcut rune) {
+
+}
+
+// handler: server incoming connection
+func handleIncomingMessage(conn net.Conn, message *tview.TextView, clientList *tview.List) {
+	defer conn.Close()
 	for {
 		var length int32
 		err := binary.Read(conn, binary.BigEndian, &length)
@@ -40,21 +47,18 @@ func handleIncomingMessage(conn net.Conn, message *tview.TextView, clientList *t
 			log.Println("Error reading message length: ", err)
 			return
 		}
-
 		buffer := make([]byte, length)
 		_, err = conn.Read(buffer)
 		if err != nil {
 			log.Println("Error reading incoming data: ", err)
 			return
 		}
-
 		var msg Message
 		err = json.Unmarshal(buffer, &msg)
 		if err != nil {
 			log.Println("Error unmarshalling message: ", err)
 			continue
 		}
-
 		prevMessage := message.GetText(true)
 		switch msg.Type {
 		case WelcomeMessage:
@@ -62,29 +66,28 @@ func handleIncomingMessage(conn net.Conn, message *tview.TextView, clientList *t
 		case ChatMessage:
 			message.SetText(prevMessage + msg.Timestamp + " >> " + msg.Payload.(string) + "\n")
 		case ClientListMessage:
-			//var _clients []string
-			//clientAddrs := msg.Payload.([]interface{})
-			//for _, addr := range clientAddrs {
-			//	_clients = append(_clients, addr.(string))
-			//}
+			clientList.Clear()
+			clientAddrs := msg.Payload.([]interface{})
+			for _, addr := range clientAddrs {
+				_clients = append(_clients, addr.(string))
+				clientList.AddItem(addr.(string), "", 0, nil)
+			}
+			clientList.SetSelectedFunc(clientSelect)
 		}
 	}
 }
 
 // connect to clichat server
-func connectToServer(welcomeBox *tview.TextView, message *tview.TextView, clientList *tview.Form) {
+func connectToServer(welcomeBox *tview.TextView, message *tview.TextView, clientList *tview.List) {
 	prevMsg := welcomeBox.GetText(true)
 	welcomeBox.SetText(prevMsg + "### Starting Connection to CLICHAT\n")
-
 	var err error
 	conn, err = net.Dial("tcp", "localhost:80")
 	if err != nil {
 		log.Fatalf("Error connecting to CLICHAT server: %v", err)
 	}
-
 	prevMsg = welcomeBox.GetText(true)
 	welcomeBox.SetText(prevMsg + "### Connected to CLICHAT server\n")
-
 	go handleIncomingMessage(conn, message, clientList)
 }
 
@@ -94,20 +97,17 @@ func messageRelay(conn net.Conn, msg string) {
 		Type:    ChatMessage,
 		Payload: msg,
 	}
-
 	data, err := json.Marshal(message)
 	if err != nil {
 		log.Print("Error marshalling message: ", err)
 		return
 	}
-
 	length := int32(len(data))
 	err = binary.Write(conn, binary.BigEndian, length)
 	if err != nil {
 		log.Print("Error writing message length: ", err)
 		return
 	}
-
 	_, err = conn.Write(data)
 	if err != nil {
 		log.Print("Error sending data to server: ", err)
@@ -116,29 +116,16 @@ func messageRelay(conn net.Conn, msg string) {
 
 func main() {
 	app := tview.NewApplication()
-
 	rightView := tview.NewFlex().SetDirection(tview.FlexRow)
-
 	// two more boxes for right view
 	welcomeView := tview.NewFlex().SetDirection(tview.FlexRow)
 	welcomeView.SetBorder(true).SetTitle("CLICHAT").SetTitleAlign(tview.AlignLeft)
-
 	leftView := tview.NewTextView()
 	leftView.SetBorder(true).SetTitle("Message").SetTitleAlign(tview.AlignLeft)
-
-	clientList := tview.NewForm()
+	clientList := tview.NewList()
 	clientList.SetBorder(true).SetTitle("Clients").SetTitleAlign(tview.AlignLeft)
-
 	root := tview.NewFlex().SetDirection(tview.FlexColumn)
-
-	messageBox := tview.NewInputField().
-		SetLabel("Input Message:  ").
-		SetFieldWidth(30).
-		SetPlaceholder(" Enter message here...").
-		SetFieldTextColor(tcell.ColorWhite).
-		SetLabelColor(tcell.ColorDarkCyan).
-		SetFieldBackgroundColor(tcell.ColorBlack).
-		SetPlaceholderTextColor(tcell.ColorYellow)
+	messageBox := tview.NewInputField().SetLabel("Input Message:  ").SetFieldWidth(30).SetPlaceholder(" Enter message here...").SetFieldTextColor(tcell.ColorWhite).SetLabelColor(tcell.ColorDarkCyan).SetFieldBackgroundColor(tcell.ColorBlack).SetPlaceholderTextColor(tcell.ColorYellow)
 	messageBox.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
 			msg := messageBox.GetText()
@@ -146,19 +133,11 @@ func main() {
 			messageBox.SetText("")
 		}
 	})
-
 	welcomeBox := tview.NewTextView()
 	connectToServer(welcomeBox, leftView, clientList)
-
-	welcomeView.AddItem(welcomeBox, 0, 1, false).
-		AddItem(messageBox, 0, 6, false)
-
-	rightView.AddItem(welcomeView, 0, 1, false).
-		AddItem(clientList, 0, 1, true)
-
-	root.AddItem(leftView, 0, 1, false).
-		AddItem(rightView, 0, 2, true)
-
+	welcomeView.AddItem(welcomeBox, 0, 1, false).AddItem(messageBox, 0, 6, false)
+	rightView.AddItem(welcomeView, 0, 1, false).AddItem(clientList, 0, 1, true)
+	root.AddItem(leftView, 0, 1, false).AddItem(rightView, 0, 2, true)
 	if err := app.SetRoot(root, true).Run(); err != nil {
 		log.Fatal("Error starting application: ", err)
 	}
--- test/test.go
+++ test/test.go
@@ -1,18 +1,29 @@
 package main
 
-import "github.com/rivo/tview"
+import (
+	"log"
+
+	"github.com/rivo/tview"
+)
 
 func main() {
-
 	app := tview.NewApplication()
-	drop := tview.NewForm().AddDropDown("", []string{"client 1", "client 2", "client 3"}, 1, selected)
+	list := tview.NewList().
+		AddItem("List item 1", "Some explanatory text", 'a', nil).
+		AddItem("List item 2", "Some explanatory text", 'b', nil).
+		AddItem("List item 3", "Some explanatory text", 'c', nil).
+		AddItem("List item 4", "Some explanatory text", 'd', nil).
+		AddItem("Quit", "Press to exit", 'q', func() {
+			app.Stop()
+		})
 
-	if err := app.SetRoot(drop, true).Run(); err != nil {
+	// Add the SetSelectedFunc to handle item selection
+	list.SetSelectedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
+		log.Printf("Selected item: %s", mainText)
+		// Add your logic here for when an item is selected
+	})
+
+	if err := app.SetRoot(list, true).SetFocus(list).Run(); err != nil {
 		panic(err)
 	}
-
-}
-
-func selected(option string, optionIndex int) {
-
 }
